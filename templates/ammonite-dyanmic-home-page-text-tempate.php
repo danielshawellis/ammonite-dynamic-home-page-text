d01bb7f5b28a3657bcce3340cbdc19c70a73ce49
Daniel Ellis <danielshawellis@gmail.com>
2020-02-28 16:26:34 -0600

    add css animation and remove unnecessary files

--- templates/ammonite-dyanmic-home-page-text-tempate.php
+++ templates/ammonite-dyanmic-home-page-text-tempate.php
@@ -1,1 +1,157 @@
-<span>This is the content returned by the Ammonite Plugin Scaffold example shortcode</span>
+<style media="screen">
+  .ammonite-dynamic-home-page-text {
+    height: 150px;
+    width: auto;
+    position: relative;
+  }
+  .ammonite-dynamic-home-page-text span {
+    position: absolute;
+    pointer-events: none;
+    z-index: 100;
+    color: white;
+    opacity: 0;
+  }
+
+  /* Element 1 */
+  .ammonite-dynamic-home-page-text span.ammonite-dynamic-home-page-text-element-1 {
+    top: 15%;
+    left: 60%;
+    animation: ammonite-dynamic-home-page-text-element-1 3.2s infinite;
+  }
+
+  @keyframes ammonite-dynamic-home-page-text-element-1 {
+    /* opacity */
+    0%    { opacity: 0; }
+    25%   { opacity: 1; }
+    90%   { opacity: 0; }
+    100%  { opacity: 0; }
+
+    /* translate and scale */
+    0%    { transform: translate( 0, 0 ) scale( 1 ); }
+    100%  { transform: translate( 200%, 300% ) scale( 2 ); }
+  }
+
+  /* Element 2 */
+  .ammonite-dynamic-home-page-text span.ammonite-dynamic-home-page-text-element-2 {
+    top: 0%;
+    left: 30%;
+    animation: ammonite-dynamic-home-page-text-element-2 4s 1.65s infinite;
+  }
+
+  @keyframes ammonite-dynamic-home-page-text-element-2 {
+    /* opacity */
+    0%    { opacity: 0; }
+    25%   { opacity: 1; }
+    90%   { opacity: 0; }
+    100%  { opacity: 0; }
+
+    /* translate and scale */
+    0%    { transform: translate( 0, 0 ) scale( 1 ); }
+    100%  { transform: translate( -200%, 100% ) scale( 2 ); }
+  }
+
+  /* Element 3 */
+  .ammonite-dynamic-home-page-text span.ammonite-dynamic-home-page-text-element-3 {
+    top: 10%;
+    left: 35%;
+    animation: ammonite-dynamic-home-page-text-element-3 3.5s 3.3s infinite;
+  }
+
+  @keyframes ammonite-dynamic-home-page-text-element-3 {
+    /* opacity */
+    0%    { opacity: 0; }
+    25%   { opacity: 1; }
+    90%   { opacity: 0; }
+    100%  { opacity: 0; }
+
+    /* translate and scale */
+    0%    { transform: translate( 0, 0 ) scale( 1 ); }
+    100%  { transform: translate( -100%, 250% ) scale( 2 ); }
+  }
+
+  /* Element 4 */
+  .ammonite-dynamic-home-page-text span.ammonite-dynamic-home-page-text-element-4 {
+    top: 5%;
+    left: 65%;
+    animation: ammonite-dynamic-home-page-text-element-4 4s 1.8s infinite;
+  }
+
+  @keyframes ammonite-dynamic-home-page-text-element-4 {
+    /* opacity */
+    0%    { opacity: 0; }
+    25%   { opacity: 1; }
+    90%   { opacity: 0; }
+    100%  { opacity: 0; }
+
+    /* translate and scale */
+    0%    { transform: translate( 0, 0 ) scale( 1 ); }
+    100%  { transform: translate( 100%, 250% ) scale( 2 ); }
+  }
+
+  /* Element 5 */
+  .ammonite-dynamic-home-page-text span.ammonite-dynamic-home-page-text-element-5 {
+    top: -5%;
+    left: 50%;
+    animation: ammonite-dynamic-home-page-text-element-5 3.6s 2.8s infinite;
+  }
+
+  @keyframes ammonite-dynamic-home-page-text-element-5 {
+    /* opacity */
+    0%    { opacity: 0; }
+    25%   { opacity: 1; }
+    90%   { opacity: 0; }
+    100%  { opacity: 0; }
+
+    /* translate and scale */
+    0%    { transform: translate( 0, 0 ) scale( 1 ); }
+    100%  { transform: translate( 50%, 350% ) scale( 2 ); }
+  }
+
+  /* Element 6 */
+  .ammonite-dynamic-home-page-text span.ammonite-dynamic-home-page-text-element-6 {
+    top: 0%;
+    left: 40%;
+    animation: ammonite-dynamic-home-page-text-element-6 4.2s 3s infinite;
+  }
+
+  @keyframes ammonite-dynamic-home-page-text-element-6 {
+    /* opacity */
+    0%    { opacity: 0; }
+    25%   { opacity: 1; }
+    90%   { opacity: 0; }
+    100%  { opacity: 0; }
+
+    /* translate and scale */
+    0%    { transform: translate( 0, 0 ) scale( 1 ); }
+    100%  { transform: translate( -350%, 380% ) scale( 2 ); }
+  }
+
+  /* Element 7 */
+  .ammonite-dynamic-home-page-text span.ammonite-dynamic-home-page-text-element-7 {
+    top: 5%;
+    left: 45%;
+    animation: ammonite-dynamic-home-page-text-element-7 4.8s 3.8s infinite;
+  }
+
+  @keyframes ammonite-dynamic-home-page-text-element-7 {
+    /* opacity */
+    0%    { opacity: 0; }
+    25%   { opacity: 1; }
+    90%   { opacity: 0; }
+    100%  { opacity: 0; }
+
+    /* translation and scale */
+    0%    { transform: translate( 0, 0 ) scale( 1 ); }
+    100%  { transform: translate( -10%, 280% ) scale( 2 ); }
+  }
+</style>
+
+<div class="ammonite-dynamic-home-page-text">
+  <span class="ammonite-dynamic-home-page-text-element-1">Health</span>
+  <span class="ammonite-dynamic-home-page-text-element-2">Wellness</span>
+  <span class="ammonite-dynamic-home-page-text-element-3">Thriving</span>
+  <span class="ammonite-dynamic-home-page-text-element-4">Family</span>
+  <span class="ammonite-dynamic-home-page-text-element-5">Longevity</span>
+  <span class="ammonite-dynamic-home-page-text-element-6">Joy</span>
+  <span class="ammonite-dynamic-home-page-text-element-7">Wisdom</span>
+</div>
